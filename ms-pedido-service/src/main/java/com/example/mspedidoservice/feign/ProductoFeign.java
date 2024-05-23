99c0d084c118cb7be8417fed5b3f062b6e52cd5c
jaqueline21623 <jaqueline.huahuaccpa@upeu.edu.pe>
2024-05-23 08:00:09 -0500

    desarrollo

--- ms-pedido-service/src/main/java/com/example/mspedidoservice/feign/ProductoFeign.java
+++ ms-pedido-service/src/main/java/com/example/mspedidoservice/feign/ProductoFeign.java
@@ -1,6 +1,5 @@
 package com.example.mspedidoservice.feign;
 
-import com.example.mspedidoservice.dto.ClienteDto;
 import com.example.mspedidoservice.dto.ProductoDto;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -8,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ms-catalogo-service",path = "/producto")
+@FeignClient(name = "ms-catalogo-service", path = "/producto")
 public interface ProductoFeign {
     @GetMapping("/{id}")
     @CircuitBreaker(name = "productoListarPorIdCB", fallbackMethod = "fallbackProductoPorId")
